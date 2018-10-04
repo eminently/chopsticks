615ba0990f9010dabb535b02632419f25fee576f
Romain Pellerin-Rezzi <romain.pellerin@gmail.com>
2018-10-03 22:12:59 -0500

    incoming nodes BU, XT, Gcash, Bcash

--- model/Blockchain.go
+++ model/Blockchain.go
@@ -9,6 +9,10 @@
 package model
 
 // types of blockchain supported
-const BLOCKCHAIN_TYPE_XBC_MAINNET = "XBC" // Bitcoin ABC / Bitcoin Unlimited
+const BLOCKCHAIN_TYPE_XBC_MAINNET = "XBC" // Bitcoin ABC
 const BLOCKCHAIN_TYPE_XBS_MAINNET = "XBS" // Bitcoin BSV
 const BLOCKCHAIN_TYPE_XBN_MAINNET = "XBN" // Bitcoin NayBC
+const BLOCKCHAIN_TYPE_XBU_MAINNET = "XBU" // Bitcoin Unlimited
+const BLOCKCHAIN_TYPE_XBX_MAINNET = "XBX" // Bitcoin XT
+const BLOCKCHAIN_TYPE_XBD_MAINNET = "XBD" // Gash bchd
+const BLOCKCHAIN_TYPE_XBB_MAINNET = "XBB" // Bcoin bcash
